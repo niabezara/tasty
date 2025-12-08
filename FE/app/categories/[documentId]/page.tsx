d9238f9b09b3d7ece4a64fb3d142210bfff0262f
Nia Bezarashvili <niabezarashvili@yahoo.com>
2025-12-08 19:11:10 +0400

    carousel styling on features page

--- FE/app/categories/[documentId]/page.tsx
+++ FE/app/categories/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import { getStrapiData } from "@/app/data-access/getStrapiData";
 import FeaturedHeader from "@/app/components/FeaturedHeader";
+import CategoryCarousel from "@/app/components/CategoryCarousel";
 
 interface PageProps {
   params: Promise<{
@@ -10,10 +11,11 @@ export default async function CategoryPage({ params }: PageProps) {
   const { documentId } = await params;
   const response = await getStrapiData(`/api/categories/${documentId}`);
   const category = response?.data || response;
-
+  console.log("Category Data:", category);
   return (
     <section>
       <FeaturedHeader category={category} />
+      <CategoryCarousel data={category.sub_collections} />
     </section>
   );
 }
--- FE/app/components/CategoryCarousel.tsx
+++ FE/app/components/CategoryCarousel.tsx
@@ -1,6 +1,7 @@
 import { StrapiImage } from "../lib/strapi-image";
 import { SubCollection } from "../types";
 import { Fragment } from "react/jsx-runtime";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 
 interface CategoryCarouselProps {
   data: SubCollection[];
@@ -8,22 +9,31 @@ interface CategoryCarouselProps {
 
 function CategoryCarousel({ data }: CategoryCarouselProps) {
   return (
-    <div>
+    <section className="py-8 md:max-w-6xl mx-auto px-4 md:px-0">
       {data?.map((subcategory) => (
         <Fragment key={subcategory?.id}>
-          <h1>`{subcategory?.subTitle} Recpies`</h1>
-          <div>
-            <StrapiImage
-              src={subcategory?.thumbnail[0]?.url}
-              alt={subcategory?.thumbnail[0]?.alternativeText || " Image"}
-              height={subcategory?.thumbnail[0]?.height}
-              width={subcategory?.thumbnail[0]?.width}
-            />
-            <h2>{subcategory?.subTitle}</h2>
-          </div>
+          <h1 className="font-sans font-bold uppercase text-[#734060] tracking-widest text-2xl text-center md:text-left  max-w-4xl md:mx-auto flex flex-wrap md:flex-nowrap justify-center items-center md:space-x-4 pb-8">
+            `{subcategory?.subTitle} Recipes`
+          </h1>
+          <Carousel>
+            <CarouselContent className="">
+              {subcategory?.recipes?.map((recipe) => (
+                <CarouselItem key={recipe.id} className="h-[375px] ">
+                  <StrapiImage
+                    src={recipe.image[0].url}
+                    alt={recipe?.name || "Recipe Image"}
+                    height={recipe.image[0].height}
+                    width={recipe.image[0].width}
+                    className="w-full h-full object-cover"
+                  />
+                  <p>{recipe.name}</p>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </Fragment>
       ))}
-    </div>
+    </section>
   );
 }
 
